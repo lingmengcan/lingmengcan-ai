0f3779ff9f522aba67030f848313686c8e77a95e
lingmengcan <52326430@qq.com>
2023-10-15 22:34:04 +0800

    优化

--- web/src/layouts/index.vue
+++ web/src/layouts/index.vue
@@ -15,56 +15,56 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import MainView from './components/main.vue';
+import { ref } from 'vue';
+import MainView from './components/main.vue';
 </script>
 
 <style lang="less" scoped>
-  .layout {
-    display: flex;
-    flex-direction: row;
-    flex: auto;
+.layout {
+  display: flex;
+  flex-direction: row;
+  flex: auto;
 
-    &-default-background {
-      background: #f5f7f9;
-    }
-
-    .layout-sider {
-      min-height: 100vh;
-      box-shadow: 2px 0 8px 0 rgb(29 35 41 / 5%);
-      position: relative;
-      z-index: 13;
-      transition: all 0.2s ease-in-out;
-    }
-
-    .layout-sider-fix {
-      position: fixed;
-      top: 0;
-      left: 0;
-    }
-
-    .ant-layout {
-      overflow: hidden;
-    }
-
-    .layout-right-fix {
-      overflow-x: hidden;
-      padding-left: 200px;
-      min-height: 100vh;
-      transition: all 0.2s ease-in-out;
-    }
-
-    .layout-content {
-      flex: auto;
-      min-height: 100vh;
-    }
-
-    .n-layout-header.n-layout-header--absolute-positioned {
-      z-index: 11;
-    }
-
-    .n-layout-footer {
-      background: none;
-    }
+  &-default-background {
+    background: #f5f7f9;
   }
+
+  .layout-sider {
+    min-height: 100vh;
+    box-shadow: 2px 0 8px 0 rgb(29 35 41 / 5%);
+    position: relative;
+    z-index: 13;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .layout-sider-fix {
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+
+  .ant-layout {
+    overflow: hidden;
+  }
+
+  .layout-right-fix {
+    overflow-x: hidden;
+    padding-left: 200px;
+    min-height: 100vh;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .layout-content {
+    flex: auto;
+    min-height: 100vh;
+  }
+
+  .n-layout-header.n-layout-header--absolute-positioned {
+    z-index: 11;
+  }
+
+  .n-layout-footer {
+    background: none;
+  }
+}
 </style>
